1536f49f740b69882cbb5932f5685c3a397ee4bb
monika <monikasharma26081991@gmail.com>
2020-02-02 13:12:07 +0530

    [Monika]Person Class Changed

--- src/main/java/Com/Lambton/Customer.java
+++ src/main/java/Com/Lambton/Customer.java
@@ -11,7 +11,7 @@ public class Customer extends  Person
 
     public Customer(int id, int age, String firstName, String lastName, String mobileNumber, String emailId, String userName,
                     VehicleManagement.GENDER gender, LocalDate birthDate, String password, String address, String city) {
-        super(id, age, firstName, lastName, mobileNumber, emailId, userName, gender, birthDate, password);
+        super(id, firstName, lastName, mobileNumber, emailId, userName, gender, birthDate, password);
         this.address = address;
         this.city = city;
     }
