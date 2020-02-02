f4b4f7ee171516fa0e31919a81459a9b62fdd269
monika <monikasharma26081991@gmail.com>
2020-02-02 15:01:29 +0530

    [Monika]Changes made in Bus Class

--- src/main/java/Com/Lambton/Bus.java
+++ src/main/java/Com/Lambton/Bus.java
@@ -1,25 +1,23 @@
 package Com.Lambton;
 
-import java.lang.reflect.Type;
-
 public class Bus extends Vehicle implements IDisplay {
 
 
-    BusType typeOfBus;
+    VehicleManagement.BusType typeOfBus;
     boolean isAccessibilityServiceAvailable;
     boolean isWifiAvailable;
 
     public Bus(long vehicleIdentificationNumber, String vehicleDescription,
                String manufacturerName, boolean isSelfDrive,
                boolean isInsured,
-               int noOfSeat, Fuel fuelType,
-               BusType typeOfBus, boolean isAccessibilityServiceAvailable,
+               int noOfSeat, VehicleManagement.FUEL fuelType,
+               VehicleManagement.BusType typeOfBus, boolean isAccessibilityServiceAvailable,
                boolean isWifiAvailable) {
         super(vehicleIdentificationNumber, vehicleDescription,
                 manufacturerName, isSelfDrive,
                 isInsured,
                 noOfSeat, fuelType);
-        super.vehicleType = Type.Bus;
+        super.vehicleType = VehicleManagement.VEHICLETYPE.BUS;
         this.typeOfBus = typeOfBus;
         this.isAccessibilityServiceAvailable = isAccessibilityServiceAvailable;
         this.isWifiAvailable = isWifiAvailable;
