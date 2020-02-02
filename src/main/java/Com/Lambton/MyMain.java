5b17cfdfa5cd6faac5dc00e51f7100a4b42822b7
monika <monikasharma26081991@gmail.com>
2020-02-02 13:34:46 +0530

    [Monika]Vehicle Class

--- src/main/java/Com/Lambton/MyMain.java
+++ src/main/java/Com/Lambton/MyMain.java
@@ -1,8 +1,46 @@
 package Com.Lambton;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.xml.crypto.Data;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+
 public class MyMain
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        //JSON parser object to parse read file
+       /* JSONParser jsonParser = new JSONParser();
+        JSONObject json;
+        try
+        {
+            FileReader reader = new FileReader("customer.json");
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
+      private static void readCustomerData(JSONObject jsonObject)
+      {
+
+      }*/
+        Customer c1 = new Customer(10,"Monika ", "Sharma","8766631257","test@gmail.com",
+                "test",VehicleManagement.GENDER.Female, LocalDate.of(1993,8,12),"test123",
+                "Brampton","Ont");
+        c1.display();
+
+
+    }
+
 }
--- src/main/java/Com/Lambton/Vehicle.java
+++ src/main/java/Com/Lambton/Vehicle.java
@@ -1,4 +1,19 @@
 package Com.Lambton;
 
+import java.util.HashMap;
+
 public class Vehicle {
+    public long vehicleIdentificationNumber;
+    private String vehicleDescription;
+    private String manufacturerName;
+    private boolean isSelfDrive;
+    private boolean isInsured;
+    public HashMap<Integer,String> driver;
+    private int noOfSeat;
+    public String prefixDetails;
+    private String insuranceProviderName;
+    private VehicleManagement.FUEL fuelType;
+    private VehicleManagement.VEHICLETYPE vehicleType;
+    public int baseRate;
+    public int ratePerKm;
 }
