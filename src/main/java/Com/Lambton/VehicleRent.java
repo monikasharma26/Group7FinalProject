8ada4868031aec1f70747d9118e25a64d2645027
monika <monikasharma26081991@gmail.com>
2020-02-02 14:19:17 +0530

    [Monika]Vehicle Rent

--- src/main/java/Com/Lambton/VehicleRent.java
+++ src/main/java/Com/Lambton/VehicleRent.java
@@ -1,4 +1,15 @@
 package Com.Lambton;
 
+import java.time.LocalDate;
+
 public class VehicleRent {
+
+    LocalDate rentStartDate;
+    LocalDate rentEndDate;
+    long rentedDays;
+    long vehicleId;
+    String vehicleName;
+    VehicleManagement.VEHICLETYPE vehicleType;
+    float noOfKmDrived;
+    float totalFare;
 }
