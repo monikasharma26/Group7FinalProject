4ca23898f5d7f1b4f324ab8bc617d7f5f8e05717
kritima <kritimakukreja5@gmail.com>
2020-02-03 12:25:36 -0500

    [KRITIMA] Variables created in VehicleSingelton class

--- src/main/java/Com/Lambton/VehicleSingelton.java
+++ src/main/java/Com/Lambton/VehicleSingelton.java
@@ -1,4 +1,13 @@
 package Com.Lambton;
 
+import java.util.ArrayList;
+
 public class VehicleSingelton {
+
+    private static VehicleSingelton ourInstance;
+    private ArrayList<VehicleRent> vehicleRentArrayList= null;
+    private double totalBill;
+
+  
 }
+
