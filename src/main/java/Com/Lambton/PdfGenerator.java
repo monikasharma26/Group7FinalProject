32fde949fcf0c4327d5bac26fb305c19056f5a21
kritima <kritimakukreja5@gmail.com>
2020-02-03 16:20:33 -0500

    [KRITIMA] logic implemented in pdf generator

--- src/main/java/Com/Lambton/PdfGenerator.java
+++ src/main/java/Com/Lambton/PdfGenerator.java
@@ -17,7 +17,30 @@ public class PdfGenerator {
                         OutputStream file = new FileOutputStream(new File("VehicleRent_Details.pdf"));
                         Document document = new Document();
                         PdfWriter.getInstance(document, file);
-                        
+
+                        PdfPTable table = new PdfPTable(1);
+                        PdfPCell cell = new PdfPCell(new Paragraph("Vehicle Rent Details"));
+                        cell.setColspan(6);
+                        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                        cell.setPadding(20.0f);
+                        cell.setSpaceCharRatio(3);
+                        cell.setBackgroundColor(new BaseColor(240, 21, 82));
+                        table.addCell(cell);
+                        for (Customer v: VehicleSingelton.getInstance().getArray()) {
+                                table.addCell(v.toString());
+                        }
+                        table.addCell("Total Bill "+String.valueOf(VehicleSingelton.getInstance().getTotalBill()));
+                        table.setSpacingBefore(50.0f);
+                        table.setSpacingAfter(50.0f);
+                        document.open();
+                        document.add(Chunk.NEWLINE);
+                        document.add(new Paragraph("Vehicle Rent"));
+                        document.add(table);
+                        document.add(Chunk.NEWLINE);
+                        document.newPage();
+                        document.close();
+                        file.close();
+                        System.out.println("Pdf created successfully..");
 
                 } catch (Exception e) {
                         e.printStackTrace();
