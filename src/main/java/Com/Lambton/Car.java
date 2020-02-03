7ff9173f9aa919e5705ae561809c4590d859d299
kritima <kritimakukreja5@gmail.com>
2020-02-03 11:49:30 -0500

    [KRITIMA] car class constructor added

--- src/main/java/Com/Lambton/Car.java
+++ src/main/java/Com/Lambton/Car.java
@@ -1,8 +1,22 @@
 package Com.Lambton;
 
+import java.lang.reflect.Type;
+
 public class Car extends Vehicle implements IDisplay{
 
     VehicleManagement.CarType carType;
     String carColor;
+
+    public Car(long vehicleIdentificationNumber, String vehicleDescription, String manufacturerName,
+               boolean isSelfDrive/*, String driver*/, boolean isInsured,/* String insuranceNameProvider,*/
+               int noOfSeat, VehicleManagement.FUEL fuelType,/* Type vehicleType, int baseRate, int ratePerKm,*/
+               VehicleManagement.CarType carType, String carColor) {
+        super(vehicleIdentificationNumber, vehicleDescription, manufacturerName,
+                isSelfDrive, /*driver,*/ isInsured, /*insuranceNameProvider,*/
+                noOfSeat, fuelType/*, vehicleType, baseRate, ratePerKm*/);
+        super.vehicleType = Type.Car;
+        this.carType = carType;
+        this.carColor = carColor;
+    }
 
 }
