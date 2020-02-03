6c5820a8b09ead75f0bbca50c14279d2489977f7
kritima <kritimakukreja5@gmail.com>
2020-02-03 16:14:04 -0500

    [KRITIMA] created pdf generator method

--- src/main/java/Com/Lambton/PdfGenerator.java
+++ src/main/java/Com/Lambton/PdfGenerator.java
@@ -1,4 +1,19 @@
 package Com.Lambton;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Date;
+
 public class PdfGenerator {
+
+        public void PdfGenrator() {
+           
 }
+
+
