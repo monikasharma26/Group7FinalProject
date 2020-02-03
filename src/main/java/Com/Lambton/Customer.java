7c507c04cea79ff1d22a6243859bcd00215826ba
kritima <kritimakukreja5@gmail.com>
2020-02-03 12:00:42 -0500

    [KRITIMA] Implemented interface in Motorcycle class

--- src/main/java/Com/Lambton/Customer.java
+++ src/main/java/Com/Lambton/Customer.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.Date;
 
 public class Customer extends  Person implements IDisplay
-    
+
 {
     String address;
     String city;
--- src/main/java/Com/Lambton/Motorcycle.java
+++ src/main/java/Com/Lambton/Motorcycle.java
@@ -1,6 +1,7 @@
 package Com.Lambton;
 
-public class Motorcycle  extends Vehicle {
+public class Motorcycle  extends Vehicle implements IDisplay{
+    
     double topSpeed;
     double milage;
     public Motorcycle(long vehicleIdentificationNumber, String vehicleDescription,
