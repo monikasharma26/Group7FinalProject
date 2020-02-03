1b3c6612cb263c2c7627b947b4897f4c056775b2
kritima <kritimakukreja5@gmail.com>
2020-02-03 02:48:32 -0500

    [KRITIMA] Bus class constructor created

--- src/main/java/Com/Lambton/Bus.java
+++ src/main/java/Com/Lambton/Bus.java
@@ -9,6 +9,22 @@ public class Bus extends Vehicle implements IDisplay {
     boolean isAccessibilityServiceAvailable;
     boolean isWifiAvailable;
 
+    public Bus(long vehicleIdentificationNumber, String vehicleDescription,
+               String manufacturerName, boolean isSelfDrive,
+               boolean isInsured,
+               int noOfSeat, Fuel fuelType,
+               BusType typeOfBus, boolean isAccessibilityServiceAvailable,
+               boolean isWifiAvailable) {
+        super(vehicleIdentificationNumber, vehicleDescription,
+                manufacturerName, isSelfDrive,
+                isInsured,
+                noOfSeat, fuelType);
+        super.vehicleType = Type.Bus;
+        this.typeOfBus = typeOfBus;
+        this.isAccessibilityServiceAvailable = isAccessibilityServiceAvailable;
+        this.isWifiAvailable = isWifiAvailable;
+    }
+
 
 
 }
