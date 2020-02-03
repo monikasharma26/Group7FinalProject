25ff7733f9ff5e39005bef1f3e3ba384f4b2e5c4
kritima <kritimakukreja5@gmail.com>
2020-02-03 12:08:48 -0500

    [KRITIMA] Constructor added in driver class

--- src/main/java/Com/Lambton/Driver.java
+++ src/main/java/Com/Lambton/Driver.java
@@ -1,9 +1,20 @@
 package Com.Lambton;
 
+import java.util.Date;
+
 public class Driver extends Person implements IDisplay {
 
     long licenceNumber;
     boolean isDrivingHistoryCleared;
     Double salary;
+
+    public Driver(int id, String firstName, String lastName, VehicleManagement.GENDER gender, Date birthDate, String mobileNumber,
+                  String emailId, String userName, String password,
+                  long licenceNumber, boolean isDrivingHistoryCleared, double salary) {
+        super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, password);
+        this.licenceNumber = licenceNumber;
+        this.isDrivingHistoryCleared = isDrivingHistoryCleared;
+        this.salary = salary;
+    }
 
 }
