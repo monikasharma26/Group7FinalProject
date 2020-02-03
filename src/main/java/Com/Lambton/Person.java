73d9017cbee75bc62339fd4c823fe3e2f1db7e13
roopvirk6 <virkikroop13@gmail.com>
2020-02-02 23:12:37 -0500

    [Roop] birthdate in person class  added

--- src/main/java/Com/Lambton/Person.java
+++ src/main/java/Com/Lambton/Person.java
@@ -1,8 +1,12 @@
 package Com.Lambton;
 
+import java.time.LocalDate;
+
 public class Person {
 
     public int id,age;
     public String firstName,lastName,mobileNumber,emailId,userName;
     public VehicleManagement.GENDER gender;
+    LocalDate birthDate;
+
 }
